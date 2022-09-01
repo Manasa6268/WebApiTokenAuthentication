dfda94e4162be6ff5d5393457edc6d846f8b2587
Manasa6268 <giddalapatimanasa@gmail.com>
2022-09-01 18:41:54 +0530

    Add project files.

---
+++ WebApiTokenAuthentication/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+namespace WebApiTokenAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetPeople()
+        {
+            return Ok("Authenticated");
+        }
+    }
+}
+
